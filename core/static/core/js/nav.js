72bcc3baa8b0b5c5955c97ec2eb592cdcf592354
gberry1291 <kuchtagary1@gmail.com>
2026-05-27 21:25:25 +0200

    3rd push

--- core/static/core/js/nav.js
+++ core/static/core/js/nav.js
@@ -63,3 +63,55 @@ if (navToggle && mobileNavPanel) {
 
 window.addEventListener("load", rebuildPriorityNav);
 window.addEventListener("resize", rebuildPriorityNav);
+
+
+const duration = 1800;
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+    anchor.addEventListener("click", function (e) {
+
+        const targetId = this.getAttribute("href");
+
+        if (!targetId || targetId === "#") return;
+
+        const target = document.querySelector(targetId);
+
+        if (!target) return;
+
+        e.preventDefault();
+
+        const targetPosition =
+            target.getBoundingClientRect().top + window.pageYOffset;
+
+        const startPosition = window.pageYOffset;
+        const distance = targetPosition - startPosition;
+
+        const duration = 1400;
+
+        let start = null;
+
+        function animation(currentTime) {
+
+            if (start === null) start = currentTime;
+
+            const timeElapsed = currentTime - start;
+
+            const progress = Math.min(timeElapsed / duration, 1);
+
+            const ease =
+                progress < 0.5
+                    ? 4 * progress * progress * progress
+                    : 1 - Math.pow(-2 * progress + 2, 3) / 2;
+
+            window.scrollTo(
+                0,
+                startPosition + distance * ease
+            );
+
+            if (timeElapsed < duration) {
+                requestAnimationFrame(animation);
+            }
+        }
+
+        requestAnimationFrame(animation);
+    });
+});
